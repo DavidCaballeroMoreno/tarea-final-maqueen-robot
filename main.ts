04ff4f9193d41b88c468428646d51cc7a851034b
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-04-04 11:29:28 +0200

    No hay cambios

--- main.ts
+++ main.ts
@@ -1,12 +1,35 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
-    }
-    if (receivedNumber == 2) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
-    }
-})
 radio.setGroup(999)
 basic.forever(function () {
-	
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 6) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        radio.sendNumber(66)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+    }
 })
