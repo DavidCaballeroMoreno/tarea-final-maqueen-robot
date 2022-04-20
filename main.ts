f66ab44df91f11b32a1d1ffef5955156fd6bf8a3
David Caballero Moreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-04-20 09:27:07 +0200

    Terminado

--- main.ts
+++ main.ts
@@ -17,11 +17,28 @@ radio.onReceivedNumber(function (receivedNumber) {
             . # . # .
             # . . . #
             `)
-    } else {
-    	
+    } else if (receivedNumber == 3) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            basic.showIcon(IconNames.Heart)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            basic.showIcon(IconNames.Yes)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            basic.showIcon(IconNames.House)
+            maqueen.motorStop(maqueen.Motors.All)
+        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+            basic.showIcon(IconNames.Giraffe)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        }
     }
 })
 radio.setGroup(999)
+basic.showString("Hola")
+music.playMelody("C D E F G A B C5 ", 120)
+music.playMelody("C5 B A G F E D C ", 120)
 basic.forever(function () {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
